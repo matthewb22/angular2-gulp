2563720ef2f298db0e8ade091e94f5325d6a6119
Matthew Berrigan <matthew@leafnode.io>
2016-08-22 18:08:25 +1200

    First pass changes - including ts files in the dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('clean', function () {
 
 // copy static assets - i.e. non TypeScript compiled source
 gulp.task('copy:assets', ['clean'], function() {
-  return gulp.src(['app/**/*', 'index.html', 'styles.css', '!app/**/*.ts'], { base : './' })
+  return gulp.src(['app/**/*', 'index.html', 'styles.css', 'systemjs.config.js', 'tsconfig.json'], { base : './' })
     .pipe(gulp.dest('dist'))
 });
 
